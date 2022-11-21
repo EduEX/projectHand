1c08844c8388d240e7d4082d6c229033ae7f36b3
Edu <eduardo.ex7@gmail.com>
2022-11-21 03:42:47 -0300

    Mudanças no arquivo whats.py

--- whats.py
+++ whats.py
@@ -1,3 +1,14 @@
 import pywhatkit
+from time import  sleep
+from datetime import datetime
 
-pywhatkit.sendwhatmsg("+558598569832", "Edu aqui, Luan", 0, 41, 12, True, 2)
+trava = 0
+
+while True:
+    agora = datetime.now()
+    sleep(0.1)
+    print(agora)
+    if(agora.hour == 3 and agora.minute == 36 and trava == 0):
+        trava = 1
+        stringEnviar = f"*{agora.day}/{agora.month}/{agora.year}*: Sistema atuando ás *{agora.hour}h{agora.minute}min*."
+        pywhatkit.sendwhats_image("+558598569832", r"C:\Users\Eduardo\Desktop\testePyrebase\pin.jpg", stringEnviar, 15, True, 3)
