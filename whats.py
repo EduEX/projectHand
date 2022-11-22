85013337c3eb634e6c139b9106e01d6034774fde
Edu <eduardo.ex7@gmail.com>
2022-11-22 00:06:34 -0300

    Código funcionando para envio de imagens com legenda para o whatsapp. Foi usado a library pywin32 no lugar de win32clipboard.

--- whats.py
+++ whats.py
@@ -8,7 +8,7 @@ while True:
     agora = datetime.now()
     sleep(0.1)
     print(agora)
-    if(agora.hour == 3 and agora.minute == 36 and trava == 0):
+    if(agora.hour == 0 and agora.minute == 1 and trava == 0):
         trava = 1
         stringEnviar = f"*{agora.day}/{agora.month}/{agora.year}*: Sistema atuando ás *{agora.hour}h{agora.minute}min*."
-        pywhatkit.sendwhats_image("+558598569832", r"C:\Users\Eduardo\Desktop\testePyrebase\pin.jpg", stringEnviar, 15, True, 3)
+        pywhatkit.sendwhats_image("+558598569832", "teste.png", stringEnviar, 15, True, 3)
